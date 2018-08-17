900a6319236899e9564869c4db04df48867dc78c
Nekmo <contacto@nekmo.com>
2018-08-17 22:16:47 +0200

    Issue #17: Allow extra parameters on Message class.

--- then/components/base.py
+++ then/components/base.py
@@ -54,7 +54,15 @@ class Component:
         if context is None:
             context = Context()
         context.update(**kwargs)
-        return self.get_class()(component=self, **context)
+        cls = self.get_class()
+        if cls._default_init:
+            fields = cls.__dataclass_fields__
+            msg_ctx = {key: context[key] for key in fields if key != 'component'}
+            message = cls(component=self, **msg_ctx)
+            message.add_extra({key: context[key] for key in context if key not in fields})
+            return message
+        else:
+            return cls(component=self, **context)
 
     def send(self, context=None, **kwargs):
         return self.message(context, **kwargs).send()
@@ -64,12 +72,17 @@ class Component:
         return self.__class__.__name__
 
 
-@dataclass
 class Message:
     component: Component = None
+    _default_init = True
+    extra = None
 
     def set_config(self, component: Component):
         self.component = component
+
+    def add_extra(self, extra):
+        self.extra = self.extra or {}
+        self.extra.update(extra)
 
     def send(self):
         raise NotImplementedError
