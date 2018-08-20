7ab2cc28b77f12e7a91d5936cf6d411300c886eb
Nekmo <contacto@nekmo.com>
2018-08-20 03:38:31 +0200

    Issue #21: Email support (default configs and auth/tls configs).

--- then/components/email.py
+++ then/components/email.py
@@ -1,11 +1,32 @@
 from __future__ import absolute_import
 
+import re
 from email.message import EmailMessage as MimeEmailMessage
 from smtplib import SMTP
+from typing import Union, List
 
 from then.components.base import split_host_port, Component, Message
 from then.types import EmailType
 from dataclasses import dataclass
+
+
+DEFAULT_CONFIGS: List[(Union[str, re._pattern_type], dict)] = [
+    ('gmail.com', {'tls': True, 'server': 'smtp.gmail.com:465'}),
+    (re.compile(r'yahoo\.[a-z]{2,4}', re.IGNORECASE), {'tls': True, 'server': 'smtp.mail.yahoo.com:587'}),
+]
+DEFAULT_CONFIGS += [
+    (re.compile(domain + r'\.[a-z]{2,4}', re.IGNORECASE), {'tls': True, 'server': 'smtp-mail.outlook.com:587'})
+    for domain in ['hotmail', 'outlook']
+]
+
+
+def get_default_config(address):
+    server = address.split('@')[1]
+    for config in DEFAULT_CONFIGS:
+        if isinstance(config[0], str) and config[0] == server:
+            return config
+        elif isinstance(config[0], re._pattern_type) and config[0].match(server):
+            return config
 
 
 @dataclass
@@ -36,6 +57,8 @@ class Email(Component):
     to: str
     from_: str = 'noreply@localhost'
     server: str = 'localhost'
+    password: str = None
+    tls: bool = False
     mode: str = 'text/plain'
 
     _message_class = EmailMessage
