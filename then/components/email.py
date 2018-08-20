5656a0ed481b098602a40725cd97cf4a34384aeb
Nekmo <contacto@nekmo.com>
2018-08-20 14:50:13 +0200

    Issue #21: Email support (auth & tls).

--- then/components/email.py
+++ then/components/email.py
@@ -2,15 +2,16 @@ from __future__ import absolute_import
 
 import re
 from email.message import EmailMessage as MimeEmailMessage
-from smtplib import SMTP
-from typing import Union, List
+from smtplib import SMTP, SMTP_SSL
+from typing import Union, List, Tuple
 
 from then.components.base import split_host_port, Component, Message
 from then.types import EmailType
 from dataclasses import dataclass
 
 
-DEFAULT_CONFIGS: List[(Union[str, re._pattern_type], dict)] = [
+DEFAULT_SERVER = 'localhost'
+DEFAULT_CONFIGS: List[Tuple[Union[str, re._pattern_type], dict]] = [
     ('gmail.com', {'tls': True, 'server': 'smtp.gmail.com:465'}),
     (re.compile(r'yahoo\.[a-z]{2,4}', re.IGNORECASE), {'tls': True, 'server': 'smtp.mail.yahoo.com:587'}),
 ]
@@ -20,13 +21,13 @@ DEFAULT_CONFIGS += [
 ]
 
 
-def get_default_config(address):
+def get_default_config(address) -> Union[dict, None]:
     server = address.split('@')[1]
     for config in DEFAULT_CONFIGS:
         if isinstance(config[0], str) and config[0] == server:
-            return config
+            return config[1]
         elif isinstance(config[0], re._pattern_type) and config[0].match(server):
-            return config
+            return config[1]
 
 
 @dataclass
@@ -47,7 +48,12 @@ class EmailMessage(Message):
 
     def send(self):
         server, port = split_host_port(self.component.server, 0)
-        s = SMTP(server, port)
+        if self.component.tls:
+            s = SMTP_SSL(server, port)
+        else:
+            s = SMTP(server, port)
+        if self.component.password:
+            s.login(self.component.from_, self.component.password)
         s.send_message(self.message, self.component.from_, self.component.to)
         s.quit()
 
@@ -56,12 +62,21 @@ class EmailMessage(Message):
 class Email(Component):
     to: str
     from_: str = 'noreply@localhost'
-    server: str = 'localhost'
+    server: str = DEFAULT_SERVER
     password: str = None
-    tls: bool = False
+    tls: Union[bool, None] = None
     mode: str = 'text/plain'
 
     _message_class = EmailMessage
+
+    def __post_init__(self):
+        default_config = get_default_config(self.from_)
+        if default_config and self.server == DEFAULT_SERVER:
+            self.server = default_config.get('server', DEFAULT_SERVER)
+        if default_config and self.tls is None:
+            self.tls = default_config.get('tls')
+        if self.tls is None:
+            self.tls = False
 
     class Validate:
         to = EmailType()
