b2779b7b93a0a095703cd929dc49bf14591264c8
Nekmo <contacto@nekmo.com>
2018-08-20 15:12:23 +0200

    Issue #21: Email support (auth & tls tests).

--- tests/components/test_email.py
+++ tests/components/test_email.py
@@ -5,7 +5,7 @@
 import unittest
 from unittest.mock import patch
 
-from then.components.email import Email, EmailMessage
+from then.components.email import Email, EmailMessage, get_default_config
 
 
 class TestEmail(unittest.TestCase):
@@ -21,9 +21,35 @@ class TestEmail(unittest.TestCase):
         self.assertEqual(message.message['from'], component.from_)
         self.assertEqual(message.message['to'], component.to)
 
+    def test_default_config(self):
+        address = 'foo@gmail.com'
+        config = get_default_config(address)
+        email = Email('bar@gmail.com', from_=address)
+        self.assertEqual(email.tls, config['tls'])
+        self.assertEqual(email.server, config['server'])
+
     @patch('then.components.email.SMTP', autospec=True)
     def test_send(self, m):
         msg = Email(self.to, server='myserver:888').message(subject=self.subject, body=self.body)
         msg.send()
         m.assert_called_with('myserver', 888)
         m.return_value.send_message.assert_called_once_with(msg.message, 'noreply@localhost', self.to)
+
+    @patch('then.components.email.SMTP', autospec=True)
+    def test_login(self, m):
+        from_ = 'myuser@foo.com'
+        password = 'password'
+        Email(self.to, from_=from_, password=password, server='myserver:888').send()
+        m.return_value.login.assert_called_once_with(from_, password)
+
+    @patch('then.components.email.SMTP_SSL', autospec=True)
+    def test_send_tls(self, m):
+        msg = Email(self.to, server='secure:465', tls=True).message(subject=self.subject, body=self.body)
+        msg.send()
+        m.assert_called_with('secure', 465)
+
+    def test_get_default_gmail_config(self):
+        self.assertIsNotNone(get_default_config('foo@gmail.com'))
+
+    def test_get_default_hotmail_config(self):
+        self.assertIsNotNone(get_default_config('foo@hotmail.es'))
