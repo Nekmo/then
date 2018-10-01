f5957c8729e7dd92331b433c5fa11ce7b9762145
Nekmo <contacto@nekmo.com>
2018-10-02 00:52:08 +0200

    Issue #23: Xiaomi Yeelight support (send test).

---
+++ tests/components/test_yeelight.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `then` package."""
+import unittest
+from unittest.mock import patch
+
+
+from then.components import Yeelight
+
+
+class TestAudio(unittest.TestCase):
+
+    def setUp(self):
+        self.patch_bulb = patch('yeelight.Bulb')
+        self.patch_logger = patch('then.components.yeelight.logger')
+        self.patch_bulb.start()
+        self.patch_logger.start()
+
+    def tearDown(self):
+        self.patch_bulb.stop()
+        self.patch_logger.stop()
+
+    def get_component(self, **kwargs):
+        return Yeelight(**kwargs)
+
+    def test_send(self):
+        from yeelight import Bulb
+
+        with patch('yeelight.discover_bulbs', return_value=[Bulb('')]) as m:
+            self.get_component().send()
