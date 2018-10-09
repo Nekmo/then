d7e9bc3fb4cc305c2ecf2dec195b316384883589
Nekmo <contacto@nekmo.com>
2018-10-09 23:59:37 +0200

    Issue #36: Yaml config

--- then/configs/yaml.py
+++ then/configs/yaml.py
@@ -1,5 +1,14 @@
 from io import TextIOWrapper
+from then.exceptions import ConfigError
 
 
 def parse_yaml(file: TextIOWrapper):
-    pass
+    from yaml import load, YAMLError
+    try:
+        from yaml import CLoader as Loader
+    except ImportError:
+        from yaml import Loader
+    try:
+        return load(file, Loader)
+    except (UnicodeDecodeError, YAMLError) as e:
+        raise ConfigError('{}'.format(e))
