ca086606a44fcfe9ed45e78cb2c32020f34873a5
Nekmo <contacto@nekmo.com>
2018-11-01 02:14:05 +0100

    Issue #42: Toml config

--- then/configs/toml.py
+++ then/configs/toml.py
@@ -1,5 +1,12 @@
 from io import TextIOWrapper
 
+from then.exceptions import ConfigError
+
 
 def parse_toml(file: TextIOWrapper):
-    pass
+    import toml
+    from toml import TomlDecodeError
+    try:
+        return toml.load(file)
+    except TomlDecodeError as e:
+        raise ConfigError('{}'.format(e))
