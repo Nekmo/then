e29a52d10f5e2ab6341e84ae02b2b3941fa001b3
Nekmo <contacto@nekmo.com>
2018-11-01 02:03:54 +0100

    Issue #39: Json config

--- then/configs/json.py
+++ then/configs/json.py
@@ -1,5 +1,12 @@
+import json
 from io import TextIOWrapper
+from json import JSONDecodeError
+
+from then.exceptions import ConfigError
 
 
 def parse_json(file: TextIOWrapper):
-    pass
+    try:
+        return json.load(file)
+    except JSONDecodeError as e:
+        raise ConfigError('{}'.format(e))
